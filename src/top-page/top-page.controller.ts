755f895366054bbfb15d059d55d57ddab401fe34
a.stepanov <a.stepanov@2035school.ru>
2023-08-19 14:37:19 +0400

    feat: добавил докер для поднятия монги

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Post } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { SaveTopPageModelDto } from './dto/save-top-page.dto';
 import { DeleteTopPageDto } from './dto/delete-top-page.dto';
 import { SuccessResponse } from '../helpers/success.response';
 import { FindTopPageResponse } from './dto/find-top-page.response';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get('get/:alias')
-  async get(@Param('alias') alias: string): Promise<TopPageModel> {}
+  async get(@Param('alias') alias: string): Promise<TopPageModel> {
+    this.configService.get('TEST');
+  }
 
   @Post('find')
   async getByCategory(@Body() dto: FindTopPageDto): Promise<FindTopPageResponse> {}
