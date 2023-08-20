69090390e225a50c4b88cf728bbfe543c9d185bc
a.stepanov <a.stepanov@2035school.ru>
2023-08-20 15:27:25 +0400

    feat: добавил методы авторизации и регистрации

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Auth, AuthSchema } from './auth.model';
+import { User, UserSchema } from './user.model';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from '../configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
   imports: [
     MongooseModule.forFeature([
       {
-        name: Auth.name,
-        schema: AuthSchema,
+        name: User.name,
+        schema: UserSchema,
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig,
+    }),
   ],
+  providers: [AuthService],
 })
 export class AuthModule {}
